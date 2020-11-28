5db93cc96770ee825ce41aab5402f88540ff1f2a
AbdelrhmanHisham <abdelrhman.hisham01@gmail.com>
2020-11-28 19:16:32 +0200

    .

--- String/Knuth–Morris–Pratt algorithm(KMP)/KMP.cpp
+++ String/Knuth–Morris–Pratt algorithm(KMP)/KMP.cpp
@@ -1,51 +1,120 @@
-// This Algorithms to find the first occurance key value
+// This Algorithms to find the occurance of pattern in a string a return key value
 #include <string>
 #include <vector>
 #include <iostream>
 using namespace std;
 
+/*************************************************/
+vector<int> Multi_KMP(string str, string needed);
 int KMP(string str, string needed);
+vector<int> faliure_fun(string str);
+/***************************************************/
+
 int main()
 {
-    string str{"dsgwadsgasdzxdsgwadsgz"};
-    string needed{"dsgwadsgz"};
+    string str{"akjnfajfc"};
+    string pattern{"ak"};
 
-    int num = KMP(str, needed);
+    int num = KMP(str, pattern);
+    cout << "single key : " << num << endl;
+
+    vector<int> vec = Multi_KMP(str, pattern);
+
+    for (int i = 0; i < vec.size(); i++)
+    {
+        printf("|%d|", vec[i]);
+    }
+
     return 0;
+}
+
+vector<int> faliure_fun(string str)
+{
+    vector<int> vec(str.size());
+    vec[0] = 0;
+    int k = 0;
+    for (int i = 1; i < str.size(); i++)
+    {
+        while (k > 0 && str[k] != str[i])
+        {
+            k = vec[k - 1];
+        }
+        if (str[k] == str[i])
+        {
+            vec[i] = ++k;
+        }
+    }
+
+    return vec;
 }
 
 int KMP(string str, string needed)
 {
+
     int n = needed.size();
-
-    int arr[n];
-
-    int i = 0;
-    int j = 1;
-
-    arr[i] = 0;
-    while (i < n && j < n)
+    if (n < 1)
     {
+        return -1;
+    }
 
-        if (needed[i] != needed[j])
+    vector<int> faliure = faliure_fun(needed);
+
+    int k = 0;
+    int key = -1;
+    for (int i = 0; i < str.size(); i++)
+    {
+        while (k > 0 && str[i] != needed[k])
         {
-            arr[j] = 0;
-            j++;
+            k = faliure[k - 1];
         }
-        else
-        {
 
-            i++;
-            arr[j] = i;
-            j++;
+        if (needed[k] == str[i])
+        {
+            k++;
+        }
+
+        if (k == n)
+        {
+            key = i - n + 1;
+            break;
         }
     }
 
-    for (int i = 0; i < n; i++)
-    {
-        printf("|%d|", arr[i]);
-    }
-    cout << endl;
-
-    return 0;
+    return key;
 }
+
+vector<int> Multi_KMP(string str, string needed)
+{
+    int n = needed.size();
+    vector<int> keys;
+    if (n < 1)
+    {
+        return keys;
+    }
+
+    vector<int> faliure = faliure_fun(needed);
+
+    int k = 0;
+    for (int i = 0; i < str.size(); i++)
+    {
+        while (k > 0 && str[i] != needed[k])
+        {
+            k = faliure[k - 1];
+        }
+
+        if (needed[k] == str[i])
+        {
+            k++;
+        }
+
+        if (k == n)
+        {
+            keys.push_back(i - n + 1);
+
+            k = faliure[k - 1];
+        }
+    }
+
+    return keys;
+}
+
